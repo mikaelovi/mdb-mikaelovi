3590fafe6ae5d2c0116311c08a768d1eacffa938
13402308 <a13402308@Mac13402308L.local>
2023-07-22 01:39:07 +0530

    add dependencies

---
+++ src/main/java/com/mikaelovi/mdbtask/MdbTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mikaelovi.mdbtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdbTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdbTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikaelovi/mdbtask/MdbTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelovi.mdbtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdbTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
